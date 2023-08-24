17d5b6eff3be10717b99004eb38967902b5b42fb
Felix Ostermann <ostermann-felix@web.de>
2023-08-24 15:21:56 +0200

    changes in mainjs

--- src/JS/main.js
+++ src/JS/main.js
@@ -14,16 +14,20 @@ Buchungytyp: ${typ}
 Betrag: ${betrag} ct
 Datum: ${datum}`);
 
-if(typ == "Einnahme")    {
-    einnahmen = einnahmen + betrag;
-    bilanz = bilanz + betrag;
-}
-
-
 let titel2 = prompt("Titel:");
 let typ2 = prompt("Typ (Einnahme oder Ausgabe):");
 let betrag2 = parseInt(prompt("Betrag: (In Eurocent)"));
 let datum2 = prompt("Datum (JJJJ-MM-TT)");
+
+if  (typ2 == "Einnahme")    {
+    einnahmen = einnahmen + betrag;
+    bilanz = bilanz + betrag2;
+}   else if (typ2 === "Ausgabe")    {
+    ausgaben = ausgaben + betrag2;
+    bilanz = bilanz + betrag2;
+}   else    {
+    console.log(`Der Typ "${typ2}" ist nicht bekannt.`);
+}
 
 console.log(`Datum: ${datum2}
 Buchungytyp: ${typ2}
