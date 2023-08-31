75a4dfa9ab2b2847d8d9905fcff703b0f209f376
Felix Ostermann <ostermann-felix@web.de>
2023-08-31 13:36:57 +0200

    if else changed to switch

--- src/JS/main.js
+++ src/JS/main.js
@@ -24,17 +24,22 @@ const haushaltsbuch = {
                     Betrag: ${this.neuer_eintrag.betrag} € Cent
                     Datum: ${this.neuer_eintrag.datum}`);
     },
-    eintrag_mit_gesamtbilanz_verrechnen()   {
-        if  (this.neuer_eintrag.typ === "Einnahme")    {
-            this.gesamtbilanz.einnahmen += this.neuer_eintrag.betrag;
-            this.gesamtbilanz.bilanz += this.neuer_eintrag.betrag;
-        }   else if (this.neuer_eintrag.typ === "Ausgabe")    {
-            this.gesamtbilanz.ausgaben -= this.neuer_eintrag.betrag;
-            this.gesamtbilanz.bilanz -= this.neuer_eintrag.betrag;
-        }   else    {
-            console.log(`Der Typ "${this.neuer_eintrag.typ}" ist nicht bekannt.`);
+    eintrag_mit_gesamtbilanz_verrechnen ()  {
+        switch (this.neuer_eintrag.typ) {
+            case "Einnahme":
+                this.gesamtbilanz.einnahmen += this.neuer_eintrag.betrag;
+                this.gesamtbilanz.bilanz += this.neuer_eintrag.betrag;
+                break;
+            case "Ausgabe":
+                this.gesamtbilanz.ausgaben -= this.neuer_eintrag.betrag;
+                this.gesamtbilanz.bilanz -= this.neuer_eintrag.betrag;
+                break;
+            default:
+                console.log(`Der Typ "${this.neuer_eintrag.typ}" ist nicht bekannt.`);
+                break;
         }
     },
+
     gesamtbilanz_ausgeben() {
         console.log(`Bilanz: ${this.gesamtbilanz.bilanz} € Cent
                     Einnahmen: ${this.gesamtbilanz.einnahmen} € Cent
