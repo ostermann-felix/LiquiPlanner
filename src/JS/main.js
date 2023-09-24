9f711dcd81df3867ad7e87433bbc3b10ef1e671e
Felix Ostermann <ostermann-felix@web.de>
2023-09-24 16:58:12 +0200

    refactoring, added arrow functions

--- src/JS/main.js
+++ src/JS/main.js
@@ -16,9 +16,7 @@ const haushaltsbuch = {
             this.eintraege.push(neuer_eintrag);
         }   else    {
             console.log("Folgende Fehler wurden gefunden:")
-            this.fehler.forEach(function(fehler) {
-                console.log(fehler);
-            });
+            this.fehler.forEach(fehler => console.log(fehler));
         }
     },
 
@@ -93,7 +91,7 @@ const haushaltsbuch = {
     },
 
     eintraege_sortieren()   {
-        this.eintraege.sort(function(eintrag_a, eintrag_b)  {
+        this.eintraege.sort((eintrag_a, eintrag_b) => {
             if  (eintrag_a.get("datum") > eintrag_b.get("datum"))  {
                 return -1;
             }   else if (eintrag_a.get("datum") < eintrag_b.get("datum")) {
@@ -143,19 +141,13 @@ const haushaltsbuch = {
     },
 
     eintraege_anzeigen()    {
-        document.querySelectorAll(".monatsliste ul").forEach(function(eintragsliste) {
-            eintragsliste.remove();
-        });
-        
+        document.querySelectorAll(".monatsliste ul").forEach(eintragsliste => eintragsliste.remove());
         let eintragsliste = document.createElement("ul");
-        for(let eintrag of this.eintraege) {
-            eintragsliste.insertAdjacentElement("befordeend", this.html_eintrag_generieren(eintrag));
-
-        }
+        this.eintraege.forEach(eintrag => 
+            eintragsliste.insertAdjacentElement("befordeend", this.html_eintrag_generieren(eintrag))
+            );
         document.querySelector(".monatsliste").insertAdjacentElement("afterbegin", eintragsliste);
     },
-
-
 
     gesamtbilanz_erstellen()    {
         let neue_gesamtbilanz = new Map();
@@ -163,7 +155,7 @@ const haushaltsbuch = {
         neue_gesamtbilanz.set("ausgaben", 0);
         neue_gesamtbilanz.set("bilanz", 0);
 
-        this.eintraege.forEach(function(eintrag)   {
+        this.eintraege.forEach(eintrag => {
             switch (eintrag.get("typ")) {
                 case "einnahme":
                     neue_gesamtbilanz.set("einnahmen", neue_gesamtbilanz.get("einnahmen") + eintrag.get("betrag"));
@@ -227,9 +219,7 @@ const haushaltsbuch = {
     },
 
     gesamtbilanz_anzeigen() {
-        document.querySelectorAll("#gesamtbilanz").forEach(function(gesamtbilanz) {
-            gesamtbilanz.remove();
-        });
+        document.querySelectorAll("#gesamtbilanz").forEach(gesamtbilanz => gesamtbilanz.remove());
         document.querySelector("body").insertAdjacentElement("beforeend", this.html_gesamtbilanz_generieren());
 
     },
