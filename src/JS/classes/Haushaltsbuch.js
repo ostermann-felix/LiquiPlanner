47d1d362d4b66764784bb29eec91858ee8bea04e
Felix Ostermann <ostermann-felix@web.de>
2023-11-22 17:13:08 +0100

    added local storage

--- src/JS/classes/Haushaltsbuch.js
+++ src/JS/classes/Haushaltsbuch.js
@@ -8,18 +8,20 @@ class Haushaltsbuch {
         this._eingabeformular = new Eingabeformular();
         this._monatslistensammlung = new Monatslistensammlung();
         this._gesamtbilanz = new Gesamtbilanz();
+        this._wiederherstellen();
     }
 
-    eintrag_hinzufuegen(formulardaten) {
+    eintrag_hinzufuegen(eintragsdaten) {
         let neuer_eintrag = new Eintrag(
-            formulardaten.titel, 
-            formulardaten.betrag, 
-            formulardaten.typ, 
-            formulardaten.datum
+            eintragsdaten.titel, 
+            eintragsdaten.betrag, 
+            eintragsdaten.typ, 
+            eintragsdaten.datum
         );
         this._eintraege.push(neuer_eintrag);
         this._monatslistensammlung.aktualisieren(this._eintraege);
         this._gesamtbilanz.aktualisieren(this._eintraege);
+        this._speichern();
     }
 
     eintrag_entfernen(timestamp) {
@@ -33,6 +35,25 @@ class Haushaltsbuch {
         this._eintraege.splice(start_index, 1);
         this._monatslistensammlung.aktualisieren(this._eintraege);
         this._gesamtbilanz.aktualisieren(this._eintraege);
+        this._speichern();
+    }
+
+    _speichern() {
+        localStorage.setItem("eintraege", JSON.stringify(this._eintraege));
+    }
+
+    _wiederherstellen() {
+        let gespeicherte_eintraege = localStorage.getItem("eintraege");
+        if (gespeicherte_eintraege !== null) {
+            JSON.parse(gespeicherte_eintraege).forEach(eintrag => {
+                this.eintrag_hinzufuegen({
+                    titel: eintrag._titel,
+                    betrag: eintrag._betrag,
+                    typ: eintrag._typ,
+                    datum: new Date(eintrag._datum)
+                });
+            });
+        }
     }
 
     start() {
